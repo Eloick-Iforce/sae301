3868a232bf5a5d904175dc10a0a52c9fbeb4e9c9
Eloïck <eloick.mickisz@protonmail.com>
2022-12-06 09:42:53 +0100

    SASS + base de données
    Comit de la base de données et du fichier avec SASS d'installer

---
+++ facture.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 06 déc. 2022 à 09:28
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `facture`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article`
+--
+
+CREATE TABLE `article` (
+  `id_article` bigint(20) UNSIGNED NOT NULL,
+  `titre_article` varchar(255) NOT NULL,
+  `chapo_article` text NOT NULL,
+  `id_categorie` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_categorie` bigint(20) NOT NULL,
+  `nom_categorie` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `elements`
+--
+
+CREATE TABLE `elements` (
+  `id_elements` bigint(20) UNSIGNED NOT NULL,
+  `balise_elements` varchar(255) NOT NULL,
+  `src` text NOT NULL,
+  `alt` varchar(255) NOT NULL,
+  `contenu` text NOT NULL,
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id_article`),
+  ADD UNIQUE KEY `id_article` (`id_article`);
+
+--
+-- Index pour la table `elements`
+--
+ALTER TABLE `elements`
+  ADD UNIQUE KEY `id_elements` (`id_elements`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id_article` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `elements`
+--
+ALTER TABLE `elements`
+  MODIFY `id_elements` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
